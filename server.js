3b82fd2b8cfddb04d66888ec1060b698af158536
Reed Stewart <reedostewart@gmail.com>
2019-07-25 16:41:58 +1200

    Setup routes for API, created User schema

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Dev Connector API running...'));
 
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
