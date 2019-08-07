5bb1260d1a28fa207ad3413ebe35bc5da7431780
Reed Stewart <reedostewart@gmail.com>
2019-08-07 12:24:41 +1200

    UI for the Posts created shows the information about the post + comments and likes

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ post: { posts, loading }, getPosts }) => {
@@ -9,10 +10,21 @@ const Posts = ({ post: { posts, loading }, getPosts }) => {
     getPosts();
   }, [getPosts]);
 
-  return (
-    <div>
-      <h1>yeet</h1>
-    </div>
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-' /> Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
